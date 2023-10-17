dfaebd3acc3118c9695c88fa291bcc806d6f6a0a
Myles Hill <mylehill99@gmail.com>
2023-10-17 11:45:29 -0400

    finished

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject");
+            var userInput = Console.ReadLine();
+
+            switch (userInput.ToLower())
+            {
+                case "science":
+                    Console.WriteLine("Science is very interesting to learn");
+                    break;
+
+                case "math":
+                    Console.WriteLine("Math is expanding throughout the world");
+                    break;
+                case "reading":
+                    Console.WriteLine("Reading is very fundamental");
+                    break;
+                case "economics":
+                    Console.WriteLine("Economics is the highest paid major");
+                    break;
+                case "english":
+                    Console.WriteLine("English is very a enjoyable subject to learn");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject");
+                    break; 
+            }
         }
+
     }
 }
